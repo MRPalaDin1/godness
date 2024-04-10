1691776929f6e3c6252613d74cdb222792cab396
MRPalaDin1 <mrpaladin228322954@gmail.com>
2024-04-10 15:35:20 +0700

    process

---
+++ pop-it-mvc/config/db.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'vkrmnupx_m6',
+    'username' => 'vkrmnupx',
+    'password' => 'tg4r5X',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
+
